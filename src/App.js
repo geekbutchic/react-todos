52ef997c3e9bb1a1979c1a8a992183d804c3f1d3
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-06-09 12:49:46 -0600

    set's up Todo List components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo List</h1>
+        <ToDoComponent />
       </header>
     </div>
   );
 }
+
+const ToDoComponent = () => {
+  const [title, setTitle] = useState("Add Todo");
+  const [description, setDescription] = useState("Description");
+  const [importance, setImportance] = useState();
+  const [submit, setSubmit] = useState();
+
+  return (
+    <div className="app-container">
+      <div id="title">
+        <h4>Title</h4>
+        <input
+          id="text-input"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            console.log(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div>
+        <h4>Description</h4>
+        <input
+          id="text-input"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+            console.log(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div>
+        <h4>Importance</h4>
+        <select
+          id="input-field-importance"
+          value={importance}
+          onChange={(e) => {
+            setImportance(e.target.value);
+            console.log(e.target.value);
+          }}
+        >
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+      </div>
+      <hr></hr>
+      <div>
+      <button
+        id="submit-button"
+        value={submit}
+        onClick={() => {
+          setSubmit("Clicked");
+          console.log(submit);
+        }}
+      >
+        SUBMIT
+      </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
